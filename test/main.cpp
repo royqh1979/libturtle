f2d69ee72425a3a655488d9ddfb2d7ecbacaf443
Roy Qu <royqh1979@gmail.com>
2022-06-02 09:16:17 +0800

    drawEllipse

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,7 @@
 typedef int xxx;
 
 int main() {
+	TurtleState ts;
     initWorld(800,600);
 //    PIMAGE back = ege::newimage(800,600);
 //    setbkcolor(WHITE,back);
@@ -11,18 +12,25 @@ int main() {
 	//setBackgroundColor(GREEN);
     //setbkcolor(WHITE);
 	
-    setSpeed(100);
+    setSpeed(500);
 	fillEllipse(100,100,20,40,RED);
 	
-	fillCircle(200,200,60,BLUE);
+	//fillCircle(200,200,60,BLUE);
+	drawEllipse(50,50,150,250);
 
     //setImmediate(TRUE);
     //setPenColor(BLACK);
-    for(int i=0;i<4;i++) {
-        fd(150);
-        lt(90);
+    for(int i=0;i<360;i++) {
+        fd(4);
+        lt(1);
     }
-
+	pu();
+	ts=getState();
+	lt(90);
+	fd(10);
+	fill(SEAGREEN);
+	setState(ts);
+	pd();
     pause();
 	clearScreen();
     hide();
